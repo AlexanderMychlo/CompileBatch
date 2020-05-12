673fa8b3eb82ada48de434253b57613f87db2234
Alexander Mychlo <alexmmych@gmail.com>
2020-05-12 18:07:15 +0100

    Added new stuff from personal project

--- CompileBatch.bat
+++ CompileBatch.bat
@@ -1,15 +1,39 @@
 @ECHO OFF
-
 SET "sourcedir=..."
+SET "librarydir=..."
+SET "includedir=..."
+
 cd %sourcedir%
 
 SETLOCAL EnableDelayedExpansion
-SET list=
-SET "space= "
 
-FOR /f "delims=" %%v IN ('dir /b /a-d "%sourcedir%\*.cpp"') DO (
-SET list=!list!%space%%%v
+SET CppList=
+SET LibList=
+SET IncludeList=
+
+SET "space= "
+SET "quote=""
+
+SET "CommandLinker=-L"
+SET "CommandLink=-l"
+SET "CommandInclude=-I"
+
+SET "extra=..."
+
+FOR /f "delims=" %%v IN ('dir /a-d /b "%sourcedir%*.cpp"') DO (
+SET CppList=!CppList!%space%%%v
 )
 
-g++%list% -o ...
+FOR /f "tokens=*" %%a IN ('dir /ad /b "%librarydir%*"') DO (
+SET LibList=!LibList!%space%%CommandLinker%%quote%!librarydir!%%a%extra%%quote%
+)
+
+FOR /f "tokens=*" %%b IN ('dir /ad /b "%includedir%*"') DO (
+IF NOT [%%b] == [ucrt] (
+SET IncludeList=!IncludeList!%space%%CommandInclude%%quote%!includedir!%%b%quote%
+ECHO !IncludeList!
+)
+)
+
+g++%CppList% -std=c++17 -mwindows %LibList% %IncludeList% -o ...
 PAUSE
